9968b2168bac48def14c3ed0952ebd7b0b78c787
Callista Stefanie Taswin <callistastefanie@gmail.com>
2022-11-27 00:27:30 -0800

    reyclebin components

---
+++ src/components/GameRecycleBins.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="recycleBinContainer">
+    <template v-if="binType == 'organic'">
+      <img src="../assets/recycling-bins/organic-bin.svg" alt="organic-bin" />
+    </template>
+    <template v-else-if="binType == 'glass'">
+      <img src="../assets/recycling-bins/glass-bin.svg" alt="glass-bin" />
+    </template>
+    <template v-else-if="binType == 'metal'">
+      <img src="../assets/recycling-bins/metal-bin.svg" alt="metal-bin" />
+    </template>
+    <template v-else-if="binType == 'paper'">
+      <img src="../assets/recycling-bins/paper-bin.svg" alt="paper-bin" />
+    </template>
+    <template v-else-if="binType == 'eWaste'">
+      <img src="../assets/recycling-bins/e-waste-bin.svg" alt="e-waste-bin" />
+    </template>
+    <template v-else-if="binType == 'plastic'">
+      <img src="../assets/recycling-bins/plastic-bin.svg" alt="plastic-bin" />
+    </template>
+    <p>{{ binType.toUpperCase() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GameRecycleBin",
+  components: {},
+  methods: {},
+  computed: {},
+  props: ["binType"],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.recycleBinContainer {
+  display: flex;
+  width: 6.25rem;
+  height: 6.5rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.938rem;
+  padding: 1rem;
+  background-color: #fff5e9;
+  transition: all .2s;
+}
+
+.recycleBinContainer:hover{
+    transform: translateY(-.2rem);
+    box-shadow: 0 .2rem 2px #cac2b8 ;
+}
+
+p {
+  font-family: "Fuzzy Bubbles", cursive;
+  font-size: 0.813rem;
+  color: black;
+  font-weight: bold;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+}
+
+</style>
--- src/views/Sandbox.vue
+++ src/views/Sandbox.vue
@@ -1,12 +1,16 @@
 <script setup>
 import GameButton from '../components/GameButton.vue'
+import GameRecycleBins from '../components/GameRecycleBins.vue';
 import Hint from '../components/Hint.vue'
 </script>
 
 <template>
   <main>
     <GameButton text="Start Game" @click="startGame" />
-    <Hint garbage-name="apple"  garbage-category="organics" />
+    <div class="recycleBins__grid">
+      <GameRecycleBins v-for="bin in recycleBins" :binType="bin" :key="bin"/>
+    </div>
+    <!-- <Hint garbage-name="apple"  garbage-category="organics" /> -->
   </main>
 </template>
 
@@ -21,7 +25,16 @@ export default {
   computed: {},
   props: {},
   data() {
-    return {};
+    return { 
+        recycleBins: [
+          'organic',
+          'glass',
+          'metal',
+          'paper',
+          'eWaste',
+          'plastic'
+        ]
+    };
   }
 };
 </script>
@@ -32,5 +45,14 @@ export default {
     flex-direction: column;
     justify-content: center;
     gap: 6rem;
+    background-color: white;
   }
+
+  .recycleBins__grid{
+    display: grid;
+    grid-template: repeat(2, 1fr)/repeat(3, 1fr);
+    grid-gap: 1.25rem;
+    place-items: center;
+  }
+
 </style>
