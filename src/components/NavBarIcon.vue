255d02ca26afbff95a75431919ffdc733ce0ef59
Laurent Beique <lbeique@outlook.com>
2022-11-26 16:55:25 -0800

    navbar

---
+++ src/components/NavBarIcon.vue
@@ -0,0 +1,66 @@
+// vue component that takes an svg path and renders it as an icon
+
+<template>
+    <div>
+        <svg
+        :width="size"
+        :height="size"
+        :viewBox="viewBox"
+        :fill="fill"
+        :stroke="stroke"
+        :stroke-width="strokeWidth"
+        :stroke-linecap="strokeLinecap"
+        :stroke-linejoin="strokeLinejoin"
+        :class="className"
+        >
+        <path :d="path" />
+        </svg>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavBarIcon",
+    props: {
+        path: {
+            type: String,
+            required: true
+        },
+        size: {
+            type: Number,
+            default: 24
+        },
+        viewBox: {
+            type: String,
+            default: "0 0 24 24"
+        },
+        fill: {
+            type: String,
+            default: "none"
+        },
+        stroke: {
+            type: String,
+            default: "currentColor"
+        },
+        strokeWidth: {
+            type: Number,
+            default: 2
+        },
+        strokeLinecap: {
+            type: String,
+            default: "round"
+        },
+        strokeLinejoin: {
+            type: String,
+            default: "round"
+        },
+        className: {
+            type: String,
+            default: ""
+        }
+    }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/NavBarLink.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <router-link to="/" class="navbar-link">Home</router-link>
+    </div>
+    
+</template>
