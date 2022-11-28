406fde6cd2b1342579c5d51150a9a4d9e89c37e4
Laurent <lbeique@outlook.com>
2022-11-27 18:04:54 -0800

    stef pull

---
+++ src/components/shared/Header.vue
@@ -0,0 +1,35 @@
+// vue component that is the header. it has a the goback button and a title
+
+<script setup>
+import GoBackVue from './GoBack.vue';
+</script>
+
+<template>
+  <header>
+    <GoBackVue :isGame="false" />
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  components: { 
+    GoBackVue 
+}
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
