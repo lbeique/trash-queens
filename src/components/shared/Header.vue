73f73dab6b0890f4a49b2a444a401232f1287ae8
Yasmina Amir <66489192+Yasminaa77@users.noreply.github.com>
2022-12-06 17:19:51 -0800

    Cards are set

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -3,52 +3,53 @@ import GoBack from './GoBack.vue';
 </script>
 
 <template>
-    <div class="header">
-        <GoBack :isGame="false" />
-        <div class="header__container">
-            <div header__title>
-                <h1>{{ title }}</h1>
-            </div>
-            
-            <slot name="Button"></slot>
-        </div>
+  <div class="header">
+    <GoBack :isGame="false" />
+    <div class="header__container">
+      <div header__text>
+        <h1>{{ title }}</h1>
+        <slot name="Desc"></slot>
+      </div>
+      <slot name="Button"></slot>
+
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Header",
-    props: {
-        title: {
-            type: String,
-            required: true
-        }
-    },
-    components: {
-        GoBack
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      required: true
     }
+  },
+  components: {
+    GoBack
+  }
 };
 </script>
 
 <style scoped>
 .header {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    width: 100%;
-    gap: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  gap: 1rem;
 }
 
 h1 {
-    font-size: 2rem;
-    font-family: "FuzzyBubbles-Regular", sans-serif;
+  font-size: 2rem;
+  font-family: "FuzzyBubbles-Regular", sans-serif;
 }
 
 .header__container {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    width:100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width:100%;
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -5,34 +5,166 @@ import Header from '../components/shared/Header.vue';
 </script>
 
 <template>
-  <Header title="About Us" />
+  <!--  <Header title="About Us" />-->
   <!-- <RouterLink to="/GamePlay-Easy">
     <Button text="Easy Mode" variant="primary" @click="startGame" />
   </RouterLink>
   <RouterLink to="/GamePlay-Hard">
     <Button text="Hard Mode" variant="primary" @click="startGame" />
   </RouterLink> -->
+  <Header title="About" >
 
+    <template class="gameIntro__content" v-slot:Desc>
+      <p>We hope that you enjoy exploring our project as much as we enjoyed making it!</p>
+    </template>
+  </Header>
+
+  <div class=" card card__appLeft">
+    <div class="card__content">
+      <h3> this is the title </h3>
+      <p>Chill on the couch table wack the mini furry mouse yet purr when give birth but be superior yet lasers are tiny mice. Always hungry. Curl into a furry donut purr like an angel but being gorgeous with belly side up so destroy couch.</p>
+    </div>
+  </div>
+
+  <div class=" card card__appRight">
+    <div class="card__content">
+      <h3> this is the title </h3>
+      <p>Chill on the couch table wack the mini furry mouse yet purr when give birth but be superior yet lasers are tiny mice. Always hungry. Curl into a furry donut purr like an angel but being gorgeous with belly side up so destroy couch.</p>
+    </div>
+  </div>
+
+  <div class=" card card__team">
+    <div class="card__content">
+      <h3> this is the title </h3>
+      <p>Chill on the couch table wack the mini furry mouse yet purr when give birth but be superior yet lasers are tiny mice. Always hungry. Curl into a furry donut purr like an angel but being gorgeous with belly side up so destroy couch, for leave fur on owners clothes.</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "About",
-    methods: {
+  name: "About",
+  methods: {
 
-    },
-    computed: {},
-    props: {},
-    data() {
-        return {};
-    },
-    components: { 
-      Header, 
-      Button 
-    }
+  },
+  computed: {},
+  props: {},
+  data() {
+    return {};
+  },
+  components: {
+    Header,
+    Button
+  }
 };
 </script>
 
 <style scoped>
+/*.page {*/
+/*  display: flex;*/
+/*  flex-direction: column;*/
+/*  align-items: center;*/
+/*  height: 100%;*/
+/*  !*padding: 2rem;*!*/
+/*  !*gap: 1rem;*!*/
+
+/*  row-gap: 1rem;*/
+/*  margin: 3rem 2rem;*/
+/*}*/
+
+
+.header {
+  padding: 2rem;
+}
+.card{
+
+  display: flex;
+  align-items: flex-end;
+  position: relative;
+  /*width: 21.875rem;*/
+  width: 360px;
+  /*height: 10.125rem;*/
+  margin-top: 2rem;
+
+  /*display: flex;*/
+  /*flex-direction: column;*/
+  /*border-radius:  8px 0 0 8px;*/
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.15));
+  /*width: 100%;*/
+  padding: 1rem;
+  /*gap: 1rem;*/
+  /*max-width: 390px;*/
+  color: white;
+  background-color: #5AB4AB;
+}
+
+.gameIntro__content {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  row-gap: 1rem;
+  margin: 3rem 2rem;
+}
+
+.card__content {
+  width: 100%;
+  padding: 2rem 1.5rem;
+  gap: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  /*align-items: center;*/
+}
+
+.card__appLeft{
+  /*align-self: flex-start;*/
+  transform: translateX(-2.5rem);
+
+  border-radius:  0 8px 8px 0;
+  padding-left: 50px;
+
+
+}
+
+.card__appRight{
+  /*align-self: flex-end;*/
+  transform: translateX(2.5rem);
+
+  border-radius:  8px 0 0 8px;
+  padding-right: 50px;
+
+}
+
+.card__team{
+  /*align-self: flex-start;*/
+  transform: translateX(-2.5rem);
+
+  border-radius:  0 8px 8px 0;
+  padding-left: 50px;
+
+
+}
+
+
+h2 {
+  font-family: "FuzzyBubbles-Regular", sans-serif;
+  color: #5ab4ab;
+  font-size: 1.5rem;
+}
+
+h3{
+  font-family: "FuzzyBubbles-Bold", sans-serif;
+}
+
+
+
+
+
+
+
+p{
+  font-family: "Nunito-Regular", sans-serif;
+  font-size: 12px;
+}
 
 </style>
